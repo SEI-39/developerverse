ffdc8fcf65d3a3779bd1bd29771749502a7108c5
tmurphy3 <tim.murphy@uconn.edu>
2020-10-07 12:24:26 -0400

    added profile page to components library

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import Profile from "./Components/Profile";
 
 export default function App() {
   return (
     <div className="App">
       Hello, world!
+      <Profile />
     </div>
   );
 }
