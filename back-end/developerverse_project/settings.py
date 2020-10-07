855e72d45ea5ed16a19c0a0ecf46ee20264d4175
iandemed <iandemed@gmail.com>
2020-10-07 10:38:15 -0400

    Added necessary files to deploy  and made changes to settings file

--- back-end/developerverse_project/settings.py
+++ back-end/developerverse_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 
 # Import enviornmental variables
 from dotenv import load_dotenv
