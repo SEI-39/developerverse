6dfeca52c963da6b3fc25d07f2eccb57c3360b55
iandemed <iandemed@gmail.com>
2020-10-09 16:31:15 -0400

    Added Project model and registered it with admin site

--- back-end/developerverse/models.py
+++ back-end/developerverse/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
+
 
 # Create your models here.
+
+class Project(models.Model):
+
+    name = models.CharField(max_length=140)
+    repo_url = models.URLField(max_length=480)
+    project_url = models.URLField(max_length=480)
+    desc = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
