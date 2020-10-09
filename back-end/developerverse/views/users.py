a9e4a1fa9e5feb2a94f087e38560f848814fde87
absurdlyeloquent <ethan.henderson397@gmail.com>
2020-10-09 11:24:54 -0400

    Rewrote user manager to require email instead of username

--- back-end/developerverse/views/users.py
+++ back-end/developerverse/views/users.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from ..models import User
+from django.conf.settings import AUTH_USER_MODEL as User
 
 def create_user(request):
     return HttpResponse("This is the create user page")
