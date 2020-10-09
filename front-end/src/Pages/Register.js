0a193cfb08b07b7540dc08d0497a5c368339b132
maurcord <67590886+maurcord@users.noreply.github.com>
2020-10-09 09:10:14 -0400

    working on login form

---
+++ front-end/src/Pages/Register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "../Components/LoginForm";
+
+function Register() {
+  //the projects will be passed props of what users profile we are on
+  return (
+    <div>
+      <h1>Login Form</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Register;
