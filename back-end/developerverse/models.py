203b7a4ff010fc749cc9a7c9ea6a864d1ccc840f
iandemed <iandemed@gmail.com>
2020-10-11 14:34:22 -0400

    Added comments and made only the name field required

--- back-end/developerverse/models.py
+++ back-end/developerverse/models.py
@@ -7,9 +7,9 @@ from users.models import User
 class Project(models.Model):
 
     name = models.CharField(max_length=140)
-    repo_url = models.URLField(max_length=480)
-    project_url = models.URLField(max_length=480)
-    desc = models.TextField(max_length=500)
+    repo_url = models.URLField(max_length=480, blank=True) # allow for longer URLs
+    project_url = models.URLField(max_length=480, blank=True)
+    desc = models.TextField(max_length=500, blank=True) # User can use about 80 words for description
 
     def __str__(self):
         return self.name
