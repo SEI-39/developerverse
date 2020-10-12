ea0783e297a4c05c0c25f46147068861fa94928d
iandemed <iandemed@gmail.com>
2020-10-12 11:27:17 -0400

    Completed and tested one-to-many project model

--- back-end/developerverse/models.py
+++ back-end/developerverse/models.py
@@ -1,15 +1,29 @@
+from django.conf import settings
 from django.db import models
-from users.models import User
-
 
 # Create your models here.
 
 class Project(models.Model):
 
-    name = models.CharField(max_length=140)
-    repo_url = models.URLField(max_length=480, blank=True) # allow for longer URLs
-    project_url = models.URLField(max_length=480, blank=True)
-    desc = models.TextField(max_length=500, blank=True) # User can use about 80 words for description
+    # We cannot directly use 
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(
+        max_length=140
+    )
+    repo_url = models.URLField(
+        max_length=480, blank=True
+    ) # allow for longer URLs
+    project_url = models.URLField(
+        max_length=480, 
+        blank=True
+    )
+    desc = models.TextField(
+        max_length=500, 
+        blank=True
+    ) # User can use about 80 words for description
 
     def __str__(self):
         return self.name
