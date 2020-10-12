1c676c0fceb86f0d3ad6e3738579ac12ddeecc63
tmurphy3 <tim.murphy@uconn.edu>
2020-10-11 22:12:19 -0400

    triple equal

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const environment = process.env.NODE_ENV;
 let instance = null;
 
-if (environment == "development") {
+if (environment === "development") {
   instance = axios.create({
     baseURL: "http://localhost:5432/",
   });
