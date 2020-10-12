4a4bb9607b660b3c545408c69e0b4240c0eb5d25
absurdlyeloquent <ethan.henderson397@gmail.com>
2020-10-12 15:20:42 -0400

    added localhost to allowed hosts and exempted create_user from csrf

--- back-end/users/views.py
+++ back-end/users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def create_user(request):
-    return HttpResponse("This is the create user page")
+    if request.method == 'POST':
+        return HttpResponse("This is the create user page")
