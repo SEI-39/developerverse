5b0b6846e17d909ceb780da7a660712383f4778c
iandemed <iandemed@gmail.com>
2020-10-12 14:15:55 -0400

    Made changes requested by Tyler on code review

--- back-end/developerverse/models.py
+++ back-end/developerverse/models.py
@@ -5,25 +5,15 @@ from django.db import models
 
 class Project(models.Model):
 
-    # We cannot directly use 
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
-    name = models.CharField(
-        max_length=140
-    )
-    repo_url = models.URLField(
-        max_length=480, blank=True
-    ) # allow for longer URLs
-    project_url = models.URLField(
-        max_length=480, 
-        blank=True
-    )
-    desc = models.TextField(
-        max_length=500, 
-        blank=True
-    ) # User can use about 80 words for description
+    # Django's documentation advises against referencing the User model directly,
+    # see following documentation for more information: 
+    # https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#referencing-the-user-model
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    name = models.CharField(max_length=140)
+    repo_url = models.URLField(max_length=480, blank=True) # allow for longer URLs
+    project_url = models.URLField(max_length=480, blank=True)
+    desc = models.TextField(max_length=3000, blank=True)
 
     def __str__(self):
         return self.name
