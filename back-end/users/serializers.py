8b352cbf4c26a8568b10d5aa77e5c90294cb1dbb
absurdlyeloquent <ethan.henderson397@gmail.com>
2020-10-13 09:45:53 -0400

    created user serializer

---
+++ back-end/users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model()(
+            email = validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
