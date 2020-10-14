8e924dbb01c7405bc4d1b61b846d6277afe04739
absurdlyeloquent <ethan.henderson397@gmail.com>
2020-10-14 10:12:49 -0400

    added local permission class

--- back-end/developerverse_project/settings.py
+++ back-end/developerverse_project/settings.py
@@ -164,7 +164,9 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
--- back-end/users/views.py
+++ back-end/users/views.py
@@ -1,11 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model, authenticate
 from .serializers import UserSerializer
-import json
+import json, os
 
+User = get_user_model()
+
+class Root(APIView):
+
+    queryset = User.objects.none()
+
+    def get(self, request):
+        return Response({'success': True, 'user': request.user})
 
 class Create(APIView):
+    # This opens the view up, so anyone can make an account
+    permission_classes = [AllowAny]
+    queryset = User.objects.none()
+
     def post(self, request):
         body = json.loads(request.body)
         serializer = UserSerializer(data={**body})
@@ -14,12 +28,11 @@ class Create(APIView):
             return Response(res)
 
 class Login(APIView):
+    permission_classes = [AllowAny]
     def get(self, request):
         body = json.loads(request.body)
         serializer = UserSerializer(data={**body})
         res = serializer.validate_login(serializer.initial_data)
         return Response(res)
 
-class Root(APIView):
-    def get(self, request):
-        return Response({'success': True, 'user': request.user})
+    queryset = User.objects.none()
