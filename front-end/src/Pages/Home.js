b55f07b0219671a338a2419af0bc0532107c054d
maurcord <67590886+maurcord@users.noreply.github.com>
2020-10-15 16:37:05 -0400

    styled home page

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "../css/Home.css";
 
 function Home() {
   return (
-    <div>
+    <div className="main-container" >
+      
       <div>
-        <h1>welcome to developerverse</h1>
-        <p>
+        <h1 className='home-title'>welcome to developerverse</h1>
+        <p className='body-text'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -15,6 +17,43 @@ function Home() {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
+      
+      <div className="main-content-container">
+        <div className="contentBox1-container">
+          <div className="picture1 uBox"></div>
+            <p className='uGray uBox'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+     
+     
+      <div className="contentBox2-container">
+        <div className="picture2 uBox"></div>
+          <p className='uGray uBox'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+     
+     <div className="contentBox3-container" >
+      <div className="picture3 uBox"></div>
+      <p className='uGray uBox'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      
+    </div>
     </div>
   );
 }
