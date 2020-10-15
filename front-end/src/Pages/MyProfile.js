8d43f2c2fbaae7754f8c38a4a03b572cfa9190df
tmurphy3 <tim.murphy@uconn.edu>
2020-10-15 17:18:55 -0400

    creating a project works

--- front-end/src/Pages/MyProfile.js
+++ front-end/src/Pages/MyProfile.js
@@ -1,17 +1,70 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import ProjectList from "../Components/ProjectList";
 import { UserContext } from "../UserContext";
+import axios from "../axios";
 
 function MyProfile() {
+  const [form, setForm] = useState(false);
+  const [name, setName] = useState("");
+  const [repoURL, setRepoURL] = useState("");
+  const [projectURL, setProjectURL] = useState("");
+  const [desc, setDesc] = useState("");
+
   const { user } = useContext(UserContext);
 
-  //the projects will be passed props of what users profile we are on
+  const createProject = async () => {
+    let headers = {
+      Authorization: `token ${user.token}`,
+    };
+    let project = {
+      user: user.user_id,
+      name: name,
+      repo_url: repoURL,
+      project_url: projectURL,
+      desc: desc,
+    };
+    await axios
+      .post(`/projects/create`, project, {
+        headers: headers,
+      })
+      .then((res) => console.log(res))
+      .catch((error) => {
+        console.log(error);
+        alert(
+          "Please make sure that the project and/or repo url is in standard url format"
+        );
+      });
 
+    setForm(false);
+  };
   return (
-    <div>
+    <div style={{ backgroundColor: "white" }}>
       <h1>My Account</h1>
       <pre>USER: {JSON.stringify(user)}</pre>
       <ProjectList />
+      <button onClick={() => setForm(true)}>create project</button>
+      {form && (
+        <div>
+          <h1>here is a form</h1>
+          <input
+            placeholder="Project Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            placeholder="Repo url"
+            onChange={(e) => setRepoURL(e.target.value)}
+          />
+          <input
+            placeholder="Project url"
+            onChange={(e) => setProjectURL(e.target.value)}
+          />
+          <input
+            placeholder="Project description"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button onClick={() => createProject()}>create</button>
+        </div>
+      )}
     </div>
   );
 }
