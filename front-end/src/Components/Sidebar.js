f8a849619ac9a87c50242884b9b5eb6c69abc26a
cblender <christopher.blender@gmail.com>
2020-10-15 22:02:45 -0400

    Implemented styling for Sidebar component.  Created Sidebar.css.

--- front-end/src/Components/Sidebar.js
+++ front-end/src/Components/Sidebar.js
@@ -2,7 +2,22 @@ import React from "react";
 import "../Styles/Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar__container uLightGray">sidebar</div>;
+  return (
+    <div className="sidebar__container uGray">
+      <div className="sideFlex">
+        <div className="sideTitle">
+          <h2 className="uLightGrayText">&lt;Sidebar&#47;&gt;</h2>
+        </div>
+        <div className="sideList">
+          <div className="dashGrid">
+            <hr className="dashLine"></hr>
+            <h2 className="uLightGrayText">List</h2>
+            <hr className="dashLine"></hr>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
