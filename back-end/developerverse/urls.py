41d05c3b1744aba1fbbefe8a34a3fa0f40b5e964
absurdlyeloquent <ethan.henderson397@gmail.com>
2020-10-15 20:57:02 -0400

    added comment paths

--- back-end/developerverse/urls.py
+++ back-end/developerverse/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('projects/', views.ProjectList.as_view(), name='project_list'),
     path('projects/create', views.ProjectCreate.as_view(), name='project_create'),
     path('projects/<int:pk>', views.ProjectDetail.as_view(), name='project_detail'),
+
+    path('projects/<int:pk>/comments', views.CommentList.as_view(), name='comment_list'),
+    path('projects/create', views.CommentCreate.as_view(), name='comment_create'),
 ]
