e9ecb7608dfc8da5da199cc5f5b5d3a8d3931f24
iandemed <iandemed@gmail.com>
2020-10-15 20:05:30 -0400

    Correctly saved permissions.py file

--- back-end/developerverse/permissions.py
+++ back-end/developerverse/permissions.py
@@ -4,11 +4,11 @@ from rest_framework import permissions
 # slightly modified. See the following link for more info:
 # https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsCreatorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed for any
         if request.method in permissions.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to the owner of the project
-        return obj.user == request.user
+        return obj.user == request.user 
